b554ea2723e1a3f391cc7982876429ef4eb1df8a
Amritendu Nath <amritendunath@yahoo.com>
2025-03-08 23:41:56 +0530

    Add: chat_ui_func

--- Front-end/chat_ui.py
+++ Front-end/chat_ui.py
@@ -7,3 +7,23 @@ agent_dict = {
     'appointment_info':'appointment_agent',
     'primary_assitant':'supervisor_agent'
 }
+
+
+def generate_uuid() -> str:
+    return str(uuid.uuid4())
+
+def make_api_call(prompt: str) -> dict:
+    """Calls the API and returns the response as a dictionary."""
+    API_URL = "http://localhost:8000/api/v1/generate-stream/"  # Replace with your actual API endpoint
+    thread_id = st.session_state.thread_id
+
+    try:
+        response = httpx.post(API_URL, json={"query": prompt}, headers={"X-THREAD-ID": thread_id}, timeout=60.0)
+        response.raise_for_status()  # Raise an error for HTTP errors
+        return response.json()  # Parse response JSON
+    except httpx.HTTPStatusError as e:
+        st.error(f"API returned an error: {e.response.status_code}")
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
+    
+    return {"answer": "Error retrieving response"}  # Fallback response
