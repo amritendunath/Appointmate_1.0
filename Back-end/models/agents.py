7ee3f3b7439f81d305f9a38fd4da3e6083632815
Amritendu Nath <amritendunath@yahoo.com>
2025-03-11 23:51:22 +0530

    Add: Created model class ToGetInfo in agents.py

--- Back-end/models/agents.py
+++ Back-end/models/agents.py
@@ -9,3 +9,19 @@ class ToPrimaryBookingAssistant(BaseModel):
     request: str = Field(
         description="Any necessary followup questions the update flight assistant should clarify before proceeding."
     )
+
+class ToGetInfo(BaseModel):
+    """Get information of doctor availability via name or specialization"""
+
+    desired_date: DateModel = Field(
+        description="The desired date for booking"
+    )
+    specialization: Optional[str] = Field(
+        default=None, description="The desired specialization of the doctor"
+    )
+    doctor_name: Optional[str] = Field(
+        default=None, description="The desired doctor name for booking"
+    )
+    request: str = Field(
+        description="Any additional information or requests from the user regarding the appointment."
+    )
