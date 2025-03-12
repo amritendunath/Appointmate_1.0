cb5ff670294adf2f729bd2af30b657d15cb127e7
Amritendu Nath <amritendunath@yahoo.com>
2025-03-12 23:54:51 +0530

    Add: Created model class for tools

--- Back-end/models/tools.py
+++ Back-end/models/tools.py
@@ -1,2 +1,14 @@
 from langchain_core.pydantic_v1 import constr, BaseModel, Field, validator
 import re
+
+class DateTimeModel(BaseModel):
+    """
+    The way the date should be structured and formatted
+    """
+    date: str = Field(..., description="Propertly formatted date", pattern=r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$')
+
+    @validator("date")
+    def check_format_date(cls, v):
+        if not re.match(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$', v):
+            raise ValueError("The date should be in format 'YYYY-MM-DD HH:MM'")
+        return v
