95a49fe6e916677aa7e2b2ab2dd3fcd635bea62f
Amritendu Nath <amritendunath@yahoo.com>
2025-03-16 23:58:30 +0530

    Updating RouterUpdater in helper

--- Back-end/tools/helper.py
+++ Back-end/tools/helper.py
@@ -10,3 +10,15 @@ class RouteUpdater:
     def __init__(self, tools,update_tool):
         self.tools = tools
         self.update_tool = update_tool
+    
+    def route_update_info(self, state: State):
+        route = tools_condition(state)
+        if route == END:
+            return END
+        tool_calls = state["messages"][-1].tool_calls
+        did_cancel = any(tc["name"] == CompleteOrEscalate.__name__ for tc in tool_calls)
+        if did_cancel:
+            return "leave_skill"
+        safe_toolnames = [t.name for t in self.tools]
+        if all(tc["name"] in safe_toolnames for tc in tool_calls):
+            return self.update_tool
