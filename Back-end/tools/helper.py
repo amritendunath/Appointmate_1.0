30ba6fdab9365adb0eeaa2616473d46ddc110362
Amritendu Nath <amritendunath@yahoo.com>
2025-03-18 00:01:17 +0530

    Updating RouterUpdater in helper

--- Back-end/tools/helper.py
+++ Back-end/tools/helper.py
@@ -92,3 +92,18 @@ def route_to_workflow(
     if not dialog_state:
         return "primary_assistant"
     return dialog_state[-1]
+
+
+def route_primary_assistant(
+    state: State,
+):
+    route = tools_condition(state)
+    if route == END:
+        return END
+    tool_calls = state["messages"][-1].tool_calls
+    if tool_calls:
+        if tool_calls[0]["name"] == ToAppointmentBookingAssistant.__name__:
+            return "enter_appointment_info"
+        else:
+            return "enter_get_info"
+    raise ValueError("Invalid route")
