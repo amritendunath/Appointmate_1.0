14f073ad2fa1f528b912f521b4954e9f2a0fd7d9
Amritendu Nath <amritendunath@yahoo.com>
2025-03-19 00:04:35 +0530

    Updating prompts

--- Back-end/utils/prompts.py
+++ Back-end/utils/prompts.py
@@ -3,3 +3,8 @@ info_agent_prompt = """You are specialized agent to provide information related 
                 For your information, Always consider current year is 2024.
                 \n\nALWAYS MAKE SURE THAT If the user needs help, and none of your tools are appropriate for it, then ALWAYS ALWAYS
                  `CompleteOrEscalate` the dialog to the primary_assistant. Do not waste the user\'s time. Do not make up invalid tools or functions."""
+
+
+booking_agent_prompt = """You are specialized agent to set, cancel or reschedule appointment based on the query. You have access to the tool.\n Make sure to ask user politely if you need any further information to execute the tool.\n For your information, Always consider current year is 2024.
+            \n\nALWAYS MAKE SURE THAT If the user needs help, and none of your tools are appropriate for it, then ALWAYS ALWAYS
+             `CompleteOrEscalate` the dialog to the primary_assistant. Do not waste the user\'s time. Do not make up invalid tools or functions."""
