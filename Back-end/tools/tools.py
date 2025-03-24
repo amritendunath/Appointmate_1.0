765ad1afae929a994189cb7b107a0ac9e6bc79cc
Amritendu Nath <amritendunath@yahoo.com>
2025-03-25 00:20:15 +0530

    Add: Created datetime converstion function

--- Back-end/tools/tools.py
+++ Back-end/tools/tools.py
@@ -3,3 +3,11 @@ from typing import  Literal
 from langchain_core.tools import tool
 import pandas as pd
 from datetime import datetime
+
+
+def convert_datetime_format(dt_str):
+    # Parse the input datetime string
+    dt = datetime.strptime(dt_str, "%Y-%m-%d %H:%M")
+    
+    # Format the output as 'DD-MM-YYYY H.M' (removing leading zero from hour only)
+    return dt.strftime("%d-%m-%Y %#H.%M")
