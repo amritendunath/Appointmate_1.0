f921ddedaa63f898894f6e1275add0367e598e8c
Amritendu Nath <amritendunath@yahoo.com>
2025-03-26 00:21:18 +0530

    Update: Created tool:check_availability_by_specialization

--- Back-end/tools/tools.py
+++ Back-end/tools/tools.py
@@ -30,3 +30,36 @@ def check_availability_by_doctor(desired_date:DateModel, doctor_name:Literal['ke
         output += "Available slots: " + ', '.join(rows)
 
     return output
+
+@tool
+def check_availability_by_specialization(desired_date:DateModel, specialization:Literal["general_dentist", "cosmetic_dentist", "prosthodontist", "pediatric_dentist","emergency_dentist","oral_surgeon","orthodontist"]):
+    """
+    Checking the database if we have availability for the specific specialization.
+    The parameters should be mentioned by the user in the query
+    """
+    #Dummy data
+    df = pd.read_csv(f"availability.csv")
+    df['date_slot_time'] = df['date_slot'].apply(lambda input: input.split(' ')[-1])
+    rows = df[(df['date_slot'].apply(lambda input: input.split(' ')[0]) == desired_date.date) & (df['specialization'] == specialization) & (df['is_available'] == True)].groupby(['specialization', 'doctor_name'])['date_slot_time'].apply(list).reset_index(name='available_slots')
+
+    if len(rows) == 0:
+        output = "No availability in the entire day"
+    else:
+        def convert_to_am_pm(time_str):
+            # Split the time string into hours and minutes
+            time_str = str(time_str)
+            hours, minutes = map(int, time_str.split("."))
+            
+            # Determine AM or PM
+            period = "AM" if hours < 12 else "PM"
+            
+            # Convert hours to 12-hour format
+            hours = hours % 12 or 12
+            
+            # Format the output
+            return f"{hours}:{minutes:02d} {period}"
+        output = f'This availability for {desired_date.date}\n'
+        for row in rows.values:
+            output += row[1] + ". Available slots: \n" + ', \n'.join([convert_to_am_pm(value)for value in row[2]])+'\n'
+
+    return output
