1a28f7918cf16ca544ceefdb0978021d994a5dce
Amritendu Nath <amritendunath@yahoo.com>
2025-03-26 00:24:32 +0530

    Update: Created tool: reschedule_appointment

--- Back-end/tools/tools.py
+++ Back-end/tools/tools.py
@@ -96,3 +96,18 @@ def set_appointment(desired_date:DateTimeModel, id_number:IdentificationNumberMo
         df.to_csv(f'availability.csv', index = False)
 
         return "Succesfully done"
+    
+@tool
+def reschedule_appointment(old_date:DateTimeModel, new_date:DateTimeModel, id_number:IdentificationNumberModel, doctor_name:Literal['kevin anderson','robert martinez','susan davis','daniel miller','sarah wilson','michael green','lisa brown','jane smith','emily johnson','john doe']):
+    """
+    Rescheduling an appointment.
+    The parameters MUST be mentioned by the user in the query.
+    """
+    df = pd.read_csv(f'availability.csv')
+    available_for_desired_date = df[(df['date_slot'] == convert_datetime_format(new_date.date))&(df['is_available'] == True)&(df['doctor_name'] == doctor_name)]
+    if len(available_for_desired_date) == 0:
+        return "Not available slots in the desired period"
+    else:
+        cancel_appointment.invoke({'date':old_date, 'id_number':id_number, 'doctor_name':doctor_name})
+        set_appointment.invoke({'desired_date':new_date, 'id_number': id_number, 'doctor_name': doctor_name})
+        return "Succesfully rescheduled for the desired time"
