4c95b5d29f0d3144c5b44dfcc9a16798be395cea
Amritendu Nath <amritendunath@yahoo.com>
2025-03-26 00:23:49 +0530

    Update: Created tool:cancel_appointment

--- Back-end/tools/tools.py
+++ Back-end/tools/tools.py
@@ -63,3 +63,19 @@ def check_availability_by_specialization(desired_date:DateModel, specialization:
             output += row[1] + ". Available slots: \n" + ', \n'.join([convert_to_am_pm(value)for value in row[2]])+'\n'
 
     return output
+
+@tool
+def cancel_appointment(date:DateTimeModel, id_number:IdentificationNumberModel, doctor_name:Literal['kevin anderson','robert martinez','susan davis','daniel miller','sarah wilson','michael green','lisa brown','jane smith','emily johnson','john doe']):
+    """
+    Canceling an appointment.
+    The parameters MUST be mentioned by the user in the query.
+    """
+    df = pd.read_csv(f'availability.csv')
+    case_to_remove = df[(df['date_slot'] == convert_datetime_format(date.date))&(df['patient_to_attend'] == id_number.id)&(df['doctor_name'] == doctor_name)]
+    if len(case_to_remove) == 0:
+        return "You don´t have any appointment with that specifications"
+    else:
+        df.loc[(df['date_slot'] == convert_datetime_format(date.date)) & (df['patient_to_attend'] == id_number.id) & (df['doctor_name'] == doctor_name), ['is_available', 'patient_to_attend']] = [True, None]
+        df.to_csv(f'availability.csv', index = False)
+
+        return "Succesfully cancelled"
