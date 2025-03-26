80ead191b46e939cfd48a2c740d45d403b75db47
Amritendu Nath <amritendunath@yahoo.com>
2025-03-27 00:27:48 +0530

    Update: Updating main file for the backend

--- Back-end/main.py
+++ Back-end/main.py
@@ -4,3 +4,12 @@ from fastapi.middleware.cors import CORSMiddleware
 from api.api_router import generate_answer_router
 import asyncio
 import uvicorn
+
+
+app = FastAPI(
+    title="Doctor's Appointment Agentic Flow",
+    version="1.0.0",
+    description="Allow users to find and book availbility",
+    openapi_url="/openapi.json",
+    docs_url="/",
+)
